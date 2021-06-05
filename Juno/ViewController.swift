e681e9495dad490f01708bea4f4c0b4a8380c855
Lesterrry <lesterrry@fetchdev.host>
2021-06-05 17:03:44 +0300

    Working

--- Juno/ViewController.swift
+++ Juno/ViewController.swift
@@ -8,10 +8,10 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    @IBOutlet weak var dummyImageView: NSImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
@@ -20,7 +20,24 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
+    
+    override func viewDidAppear() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.spinDisk()
+        }
+    }
 
-
+    func spinDisk() {
+        let rotateAnimation = CABasicAnimation(keyPath: "transform.rotation")
+        rotateAnimation.fromValue = 0.0
+        rotateAnimation.byValue = Double.pi * -2.0
+        rotateAnimation.duration = 1
+        rotateAnimation.repeatCount = .infinity
+        
+        dummyImageView.layer?.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        dummyImageView.layer?.position = CGPoint(x: dummyImageView.frame.origin.x + dummyImageView.frame.width / 2,
+                                             y: dummyImageView.frame.origin.y + dummyImageView.frame.height / 2)
+        dummyImageView.layer?.add(rotateAnimation, forKey: nil)
+    }
 }
 
